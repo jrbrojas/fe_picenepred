5ac1578536f1e9631d200ad73362e0b758536af4
Leodanperez <leodanperez.1998@gmail.com>
2025-09-02 20:59:51 -0500

    Subiendo avances

--- starter/src/views/modules/dimse/SingleMenuView.tsx
+++ starter/src/views/modules/dimse/SingleMenuView.tsx
@@ -2,15 +2,31 @@
 const SingleMenuView = () => {
     return (
         <div className="relative w-full" style={{ paddingTop: '56.25%' }}>
-            <iframe
-                title="Power BI Report"
-                src="https://app.powerbi.com/view?r=eyJrIjoiMjE1N2JiYmMtYjNjNS00ZjBlLWFjN2ItODdiYmU3NTBkNWIzIiwidCI6IjM2MDJjOGY2LTQ3NzgtNGQwYi04OTczLTY5MTczYjE3N2U4YiIsImMiOjR9"
+            <img
+                src="/img/others/img2.jpeg"
                 className="absolute top-0 left-0 w-full h-full rounded-lg shadow-lg"
                 loading="lazy"
-                allowFullScreen
             />
         </div>
     )
 }
 
 export default SingleMenuView
+
+/*
+ <iframe
+                title="Power BI Report"
+                src="https://app.powerbi.com/view?r=eyJrIjoiMjE1N2JiYmMtYjNjNS00ZjBlLWFjN2ItODdiYmU3NTBkNWIzIiwidCI6IjM2MDJjOGY2LTQ3NzgtNGQwYi04OTczLTY5MTczYjE3N2U4YiIsImMiOjR9"
+                className="absolute top-0 left-0 w-full h-full rounded-lg shadow-lg"
+                loading="lazy"
+                allowFullScreen
+            />
+
+            <div className="grid h-[70vh] md:h-[85vh] w-full place-items-center bg-slate-100 p-2">
+            <img
+                src="/img/others/img2.jpeg"
+                className="h-full w-auto object-contain"
+                alt=""
+            />
+        </div>
+            */
