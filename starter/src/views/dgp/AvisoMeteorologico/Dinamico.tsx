a82f7a2821cc1b8dbd3e272953fbbcb268c7b08d
Kenyi Valladolid <valladolidk03@gmail.com>
2025-10-04 12:53:06 -0500

    :rocket: Update login user

--- starter/src/views/dgp/AvisoMeteorologico/Dinamico.tsx
+++ starter/src/views/dgp/AvisoMeteorologico/Dinamico.tsx
@@ -2,9 +2,7 @@ const LluviasAvisoMeteorologicoDinamico = () => {
     return (
         <div className="bg-white rounded-lg shadow-md p-4">
             <div className="w-full h-[80vh]" >
-                <iframe title="Formulario Lluvias Aviso Meteorologico"
-                        className="w-full h-full border-0"
-                    src="https://app.powerbi.com/view?r=eyJrIjoiNjMyNGNiZTktOGNkYS00NGNiLWE4MDMtODU2NjAyOTEwMjBlIiwidCI6IjM2MDJjOGY2LTQ3NzgtNGQwYi04OTczLTY5MTczYjE3N2U4YiIsImMiOjR9&pageName=f1fdb54c705e20b43af4" allowFullScreen={true}></iframe>
+                <iframe title="Formulario Lluvias Aviso Meteorologico (1)" width="100%" height="100%" src="https://app.powerbi.com/view?r=eyJrIjoiMDI2MjUxN2QtMmI1YS00MmE5LWFmZTAtYmVmOTIyMzEzN2ZjIiwidCI6IjM2MDJjOGY2LTQ3NzgtNGQwYi04OTczLTY5MTczYjE3N2U4YiIsImMiOjR9" allowFullScreen={true}></iframe>
             </div>
         </div>
     )
