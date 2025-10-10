d81d4ce90e9f2c4ccb5cf879748f5a3d4bc1bdcc
Kenyi Valladolid <valladolidk03@gmail.com>
2025-10-10 18:39:49 -0500

    update Modelo dinamico

--- starter/src/views/dgp/AvisoMeteorologico/Dinamico.tsx
+++ starter/src/views/dgp/AvisoMeteorologico/Dinamico.tsx
@@ -2,7 +2,7 @@ const LluviasAvisoMeteorologicoDinamico = () => {
     return (
         <div className="bg-white rounded-lg shadow-md p-4">
             <div className="w-full h-[80vh]" >
-                <iframe title="Formulario Lluvias Aviso Meteorologico (1)" width="100%" height="100%" src="https://app.powerbi.com/view?r=eyJrIjoiMDI2MjUxN2QtMmI1YS00MmE5LWFmZTAtYmVmOTIyMzEzN2ZjIiwidCI6IjM2MDJjOGY2LTQ3NzgtNGQwYi04OTczLTY5MTczYjE3N2U4YiIsImMiOjR9" allowFullScreen={true}></iframe>
+                <iframe title="lluvias_aviso_meteorologico" width="100%" height="100%" src="https://app.powerbi.com/view?r=eyJrIjoiZDNkMmE0NjEtNTNjZC00ZGVjLTgwMDctOGVjODJkZDliODQ1IiwidCI6IjM2MDJjOGY2LTQ3NzgtNGQwYi04OTczLTY5MTczYjE3N2U4YiIsImMiOjR9" allowFullScreen={true}></iframe>
             </div>
         </div>
     )
--- starter/src/views/dgp/AvisoTrimestral/Dinamico.tsx
+++ starter/src/views/dgp/AvisoTrimestral/Dinamico.tsx
@@ -2,7 +2,7 @@ const LluviasAvisoMeteorologicoDinamico = () => {
     return (
         <div className="bg-white rounded-lg shadow-md p-4">
             <div className="w-full h-[80vh]" >
-                <iframe title="Formulario Lluvias Aviso Meteorologico (1)" width="100%" height="100%" src="https://app.powerbi.com/view?r=eyJrIjoiMDI2MjUxN2QtMmI1YS00MmE5LWFmZTAtYmVmOTIyMzEzN2ZjIiwidCI6IjM2MDJjOGY2LTQ3NzgtNGQwYi04OTczLTY5MTczYjE3N2U4YiIsImMiOjR9" allowFullScreen={true}></iframe>
+                <iframe title="Lluvias Aviso Trimestral" width="100%" height="100%" src="https://app.powerbi.com/view?r=eyJrIjoiMjJjN2ZkNzAtYTQ0Zi00ZWI2LThhMzMtOTc5NTE5MzZiNjk4IiwidCI6IjM2MDJjOGY2LTQ3NzgtNGQwYi04OTczLTY5MTczYjE3N2U4YiIsImMiOjR9" allowFullScreen={true}></iframe>
             </div>
         </div>
     )
