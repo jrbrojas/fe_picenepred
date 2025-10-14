2bf1ec420a8b70eae4c16ac78f01f677fe72fb56
Kenyi Valladolid <valladolidk03@gmail.com>
2025-10-13 19:15:45 -0500

    :rocket: Update Modelos Dinamicos

--- starter/src/views/dgp/AvisoMeteorologico/Dinamico.tsx
+++ starter/src/views/dgp/AvisoMeteorologico/Dinamico.tsx
@@ -3,6 +3,7 @@ const LluviasAvisoMeteorologicoDinamico = () => {
         <div className="bg-white rounded-lg shadow-md p-4">
             <div className="w-full h-[80vh]" >
                 <iframe title="lluvias_aviso_meteorologico" width="100%" height="100%" src="https://app.powerbi.com/view?r=eyJrIjoiZDNkMmE0NjEtNTNjZC00ZGVjLTgwMDctOGVjODJkZDliODQ1IiwidCI6IjM2MDJjOGY2LTQ3NzgtNGQwYi04OTczLTY5MTczYjE3N2U4YiIsImMiOjR9" allowFullScreen={true}></iframe>
+
             </div>
         </div>
     )
