06f3d6bcb718818902321dbd18bd31d7bcffdf8b
Kenyi Valladolid <valladolidk03@gmail.com>
2025-10-18 22:42:44 -0500

    :rocket: Update 4 ultimos escenario

--- starter/src/views/dgp/BajasTempAvisoTrimestral/Dinamico.tsx
+++ starter/src/views/dgp/BajasTempAvisoTrimestral/Dinamico.tsx
@@ -2,7 +2,7 @@ const LluviasAvisoMeteorologicoDinamico = () => {
     return (
         <div className="bg-white rounded-lg shadow-md p-4">
             <div className="w-full h-[80vh]" >
-                <iframe title="Bajas Temp Aviso Meteorologico" width="100%" height="100%" src="https://app.powerbi.com/view?r=eyJrIjoiYzFmNGNiZDktYjhmYy00ZDAyLWExMGQtODMzYzkwNzNlNDQ2IiwidCI6IjM2MDJjOGY2LTQ3NzgtNGQwYi04OTczLTY5MTczYjE3N2U4YiIsImMiOjR9" allowFullScreen={true}></iframe>
+                <iframe title="5.Bajas Temp Aviso Trimestral" width="100%" height="100%" src="https://app.powerbi.com/view?r=eyJrIjoiMjFhNDc3MTAtNmVlYi00MThjLWEzNTYtZjU5ZGRlMDUxNWE3IiwidCI6IjM2MDJjOGY2LTQ3NzgtNGQwYi04OTczLTY5MTczYjE3N2U4YiIsImMiOjR9" allowFullScreen={true}></iframe>
             </div>
         </div>
     )
