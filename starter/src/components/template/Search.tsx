68ba9eb572931bac495137c6319b2601275625a9
Erick Tucto <erick@ericktucto.com>
2025-11-11 15:15:15 -0500

    arreglo de tooltip

--- starter/src/components/template/Search.tsx
+++ starter/src/components/template/Search.tsx
@@ -9,6 +9,7 @@ import { PiMagnifyingGlassDuotone } from 'react-icons/pi'
 import Highlighter from 'react-highlight-words'
 import apiGet from '@/services/ApiServiceSearch'
 import basicos from './data/search/basicos'
+import { Tooltip } from 'react-tooltip'
 
 interface ResultadoBasico {
     base_datos: 'frontend';
@@ -248,24 +249,34 @@ const _Search = ({ className }: { className?: string }) => {
       )
     }
   }
-      
   return (
     <>
+        <Tooltip
+            id="home-tooltip"
+            style={{ zIndex: 1000 }}
+            opacity={1}
+            place="top"
+            offset={0}
+        />
       <div
         ref={inputRef}
         id="search"
+        data-tooltip-id='home-tooltip'
+        data-tooltip-content='Buscar escenarios de riesgo, informes, capacitaciones, supervisiones o reportes!'
         className={classNames(
           className,
           'relative text-2xl cursor-pointer hidden lg:block xl:w-120 2xl:w-140'
         )}
         style={{ backgroundColor: '#eaeaea92' }}
       >
-        <div className="flex items-center flex-1 pe-2">
+        <div
+            className="flex items-center flex-1 pe-2"
+        >
           <input
             list="suggestionsList"
             id="inputsearch"
             className="ring-0 outline-none block w-full px-3 py-2 text-base bg-transparent text-gray-900 dark:text-gray-100"
-            placeholder="Buscar informes, capacitaciones o supervisiones..."
+            placeholder="Buscar..."
             value={query}
             onChange={handleChange}
             onKeyDown={handleKeyDown}
@@ -305,13 +316,15 @@ const _Search = ({ className }: { className?: string }) => {
       >
         <div className="bg-white dark:bg-gray-800 flex flex-col rounded-xl max-h-[80vh] overflow-hidden">
 
-          <div className="flex items-center mx-5 my-3 justify-between border-b border-gray-200 dark:border-gray-700">
+          <div
+            className="flex items-center mx-5 my-3 justify-between border-b border-gray-200 dark:border-gray-700"
+          >
             <div className="flex items-center flex-1">
               <HiOutlineSearch className="text-xl text-gray-500" />
               <input
                 ref={inputRef}
                 className="ring-0 outline-none block w-full p-4 text-base bg-transparent text-gray-900 dark:text-gray-100"
-                placeholder="Buscar informes, capacitaciones o supervisiones..."
+                placeholder="Buscar..."
                 value={query}
                 onChange={(e) => {
                     setQuery(e.target.value)
